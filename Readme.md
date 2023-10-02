f9ca1bd552b49c22a286282c6d3940f23176f55d
vaibhav-kundaliya <141109310+vaibhav-kundaliya@users.noreply.github.com>
2023-10-02 14:50:01 +0530

    build: initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+### DFTM decoder
+
+<b>Link:</b>  
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import os
+from DTMFdetector import DTMFdecoder
+app = Flask(__name__)
+
+@app.route("/")
+def homePage():
+    return  render_template("home.html")
+
+@app.route("/uploadFile", methods=['POST'])
+def extractAudio():
+    if request.method=="POST":
+        try:
+            audioFile = request.files["fileInput"]
+            fileName = audioFile.filename
+            audioFile.save(fileName)
+            data = DTMFdecoder(fileName)
+            print(data)
+            os.remove(fileName)
+            return render_template("home.html", data={"number": data})
+        except Exception as exe:
+            return render_template("home.html", error={"error":exe})
+if __name__ == "__main__":
+    app.run()
